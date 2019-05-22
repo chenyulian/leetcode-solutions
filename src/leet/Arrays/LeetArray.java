2b6b4343bc7695807575b4f31e35d842553a2842
trilliean <648270560@qq.com>
2019-05-22 17:40:43 +0800

    改进了代码，规定：输入的数组，第一个元素，其左侧的数字之和0，最后一个元素，其右侧的数字之和为0

--- src/leet/Arrays/LeetArray.java
+++ src/leet/Arrays/LeetArray.java
@@ -1,5 +1,8 @@
 package leet.Arrays;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class LeetArray {
 
     public LeetArray(){
@@ -21,16 +24,26 @@ public class LeetArray {
         }
 
         int result = -1;
-        for(int i = 1; i < nums.length-1; i ++) {
+        for(int i = 0; i < nums.length; i ++) {
             int sumLeft = 0;
             int sumRight = 0;
             for(int j = i-1; j>=0;j--) {
-                //遍历左侧数
-                sumLeft += nums[j];
+                //第一个数的左侧数之和记为0
+                if(j < 0) {
+                    sumLeft = 0;
+                }else {
+                    //遍历左侧数
+                    sumLeft += nums[j];
+                }
             }
             for(int k = i+1; k < nums.length; k ++) {
                 //遍历右侧数
-                sumRight += nums[k];
+                if(k > nums.length){
+                    //最后一个数的右侧数之和记为0
+                    sumRight = 0;
+                }else {
+                    sumRight += nums[k];
+                }
             }
             if(sumLeft == sumRight) {
                 result = i;
@@ -44,10 +57,29 @@ public class LeetArray {
      * @param args
      */
     public static void main(String[] args) {
-        int test[] = {1,1,2,5,11,2,1,5,3,1,13,6};
-        LeetArray arrays = new LeetArray();
-        int result = arrays.pivotIndex(test);
-        System.out.print("中心索引：" + result);
+        //int test[] = {1,1,2,5,11,2,1,5,3,1,13,6};
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.println("请输入数组：（如[1,2,11,4]）");
+            String testStr = bufferedReader.readLine();
+            if(testStr != null && testStr.length() > 0){
+
+                String[] temp = testStr.substring(1,testStr.length()-1).split(",");
+                int[] test = new int[temp.length];
+
+                for (int i = 0; i < temp.length; i++) {
+                    test[i] = Integer.valueOf(temp[i]);
+                }
+                LeetArray arrays = new LeetArray();
+                int result = arrays.pivotIndex(test);
+                System.out.print("中心索引：" + result);
+            }
+
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
