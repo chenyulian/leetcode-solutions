146fab5879138b3f6e96fb3abe1222d14b76c610
chenyulian <648270560@qq.com>
2019-11-28 23:24:12 +0800

    initial ListNode

---
+++ src/leet/linkedlist/ListNode.java
@@ -0,0 +1,17 @@
+package linkedlist;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+        val = x;
+    }
+
+    /**
+     * 单链表反转 LC@206
+     * @return
+     */
+    public  ListNode reverseList() {
+        return null;
+    }
+}
