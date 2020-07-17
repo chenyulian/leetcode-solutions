62e52fd83e98c8d932a8c578e9b20adde5bed9d2
kinppink <648270560@qq.com>
2020-07-17 18:13:29 +0800

    :tada: Add solution for leetcode 700

---
+++ 700-Search-In-A-Binary-Search-Tree/src/Solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+
+    //Definition for a binary tree node.
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null) {
+            return null;
+        }
+
+        if(root.val == val) {
+            return root;
+        }else if(root.val < val) {
+            return searchBST(root.right, val);
+        }else{
+            return searchBST(root.left, val);
+        }
+    }
+
+}
